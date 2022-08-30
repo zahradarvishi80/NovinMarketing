43779314b7e265bd594f29d41bf9cdd89078500b
zahradarvishi80 <zahra.darvishi13802000@gmail.com>
2022-08-30 14:10:10 +0430

    change bg xl

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CarouselSlid from "./Component/CarouselSlider";
 import Describtion from "./Component/Describtion";
 function App() {
   return (
-    <div className="flex max-w-md mx-auto flex-col bg-white container sm:max-w-xl  md:max-w-3xl  lg:max-w-4xl  xl:max-w-6xl 2xl:max-w-20xl 2xl:bg-orange-400">
+    <div className="flex max-w-md mx-auto flex-col bg-white container sm:max-w-xl  md:max-w-3xl  lg:max-w-4xl  xl:max-w-6xl 2xl:max-w-20xl">
     <SlikSlider/>
     <CarouselSlid/>  
     <Describtion/>
